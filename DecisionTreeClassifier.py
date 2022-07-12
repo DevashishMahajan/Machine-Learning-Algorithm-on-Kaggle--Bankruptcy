bbdd2e192040c31c70c88ca856715895964d0565
Devashish <32193655+DevashishMahajan@users.noreply.github.com>
2022-07-12 20:08:10 +0530

    Decision Tree Classifier
     Decision Tree Classifier comments added

--- DecisionTreeClassifier.py
+++ DecisionTreeClassifier.py
@@ -5,7 +5,7 @@ Created on Tue May 17 11:44:01 2022
 @author: Devashish
 """
 
-
+# Import necessary libraries 
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
@@ -15,20 +15,30 @@ from sklearn import tree
 
 import warnings
 warnings.filterwarnings("ignore")
+# Change the file path as the file path in your computer
 import os
 os.chdir("D:\CDAC ML\Cases\Bankruptcy")
 
 
+# Read csv files from kaggle dataset as Pandas Dataframe
 df = pd.read_csv("Bankruptcy.csv")
+
+# X is a feature 
 x=df.iloc[:,2:]
+
+#y is a label
 y=df.iloc[:,1]
 
-
+#### Decision Tree Classifier ####
 clf = DecisionTreeClassifier(random_state=2022)
+
 kfold = StratifiedKFold(n_splits=5,shuffle=True,random_state=2022)
+
 params = {'max_depth':[3,4,None],'min_samples_split':[2,10,20],'min_samples_leaf':[1,5,10]}
+
 gcv = GridSearchCV(clf,scoring='roc_auc',cv=kfold,param_grid=params)
 gcv.fit(x,y)
+
 print(gcv.best_params_)
 print(gcv.best_score_)
 
